42c95f4d8db3bb5aff52ba9ccc4e2a44205d1b6d
GAME1.1 <GAME1.1@etpa.local>
2020-11-17 17:30:55 +0100

    Update Tri.py

--- Tri.py
+++ Tri.py
@@ -1,8 +1,8 @@
 print("Hello world")
 
+#Tri par Selection
 def tri_selection(tab):
    for i in range(len(tab)):
-      # Trouver le min
        min = i
        for j in range(i+1, len(tab)):
            if tab[min] > tab[j]:
@@ -12,10 +12,46 @@ def tri_selection(tab):
        tab[i] = tab[min]
        tab[min] = tmp
    return tab
-# Programme principale pour tester le code ci-dessus
 tab = [98, 22, 15, 32, 2, 74, 63, 70]
  
-tri_selection(tab)
- 
-print ("Le tableau trié est:")
-print (tab)
+print ("Le tableau trié par Selection est:")
+print(tab)
+print (tri_selection(tab))
+
+#Tri par fusion
+list=[1,4,6,8,0,3,9,2,5]
+
+def fusion(left, right):
+    res= []
+    i=0
+    j=0
+
+    while i<len(left) and j<len(right):
+        if(left[i] < right[j]):
+            res.append(left[i])
+            i=i+1
+        else:
+            res.append(right[j])
+            j=j+1
+    if i==len(left):
+        res.extend(right[j:])
+    if j==len(right):
+        res.extend(left[i:])
+    return res
+
+def tri_fusion(tab, start, end):
+    if ((len(tab))<=1):
+        return tab
+    else:
+        milieu =(start + end)//2
+        left = tab[:milieu]
+        right = tab[milieu:]
+
+        left = tri_fusion(left, 0, len(left))
+        right = tri_fusion(right, 0,len(right))
+
+        return fusion(left, right)
+
+print("Le tableau trié par Fusion est:")
+print(list)
+print(tri_fusion(list, 0, len(list)))
